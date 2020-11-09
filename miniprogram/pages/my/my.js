5b399f4a93e71dbf78944d0df9aa5afa92667de0
路明 <snaillu@yeah.net>
2020-11-09 16:32:37 +0800

    个人中心页面开发

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -5,7 +5,74 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {
+      name: '发了芽的土豆',
+      currentPoint: 780,
+      totalPoint: 1000,
+      cardNo: 'WX2020101010',
+      memberLabel: '钻石会员',
+      orderStatusCountMap: {
+        waitPayCount: 0,
+        waitShipCount: 12,
+        waitReceiveCount: 120,
+        waitCommentCount: 19,
+        refundCount: 7
+      }
+    },
+    orderMenuList: [
+      {
+        label: '待付款',
+        icon: 'unpay',
+        countField: 'waitPayCount',
+        pageTarget: '/pages/orderList/orderList'
+      },
+      { 
+        label: '待发货',
+        icon: 'unship',
+        countField: 'waitShipCount',
+        pageTarget: '/pages/orderList/orderList'
+      }, 
+      { 
+        label: '待收货',
+        icon: 'unreceive',
+        countField: 'waitReceiveCount',
+        pageTarget: '/pages/orderList/orderList'
+      },
+      { 
+        label: '待评价',
+        icon: 'uncomment',
+        countField: 'waitCommentCount',
+        pageTarget: '/pages/orderList/orderList'
+      },
+      { 
+        label: '退款/售后',
+        icon: 'refund',
+        countField: 'refundCount',
+        pageTarget: '/pages/refundOrderList/refundOrderList'
+      }
+    ],
+    serviceMenuList: [
+      {
+        label: '优惠券',
+        icon: 'coupon',
+        pageTarget: '/pages/couponList/couponList'
+      },
+      { 
+        label: '收藏夹',
+        icon: 'favorite',
+        pageTarget: '/pages/favorites/favorites'
+      }, 
+      { 
+        label: '个人资料',
+        icon: 'person',
+        pageTarget: '/pages/personInfo/personInfo'
+      },
+      { 
+        label: '地址簿',
+        icon: 'address',
+        pageTarget: '/pages/addressList/addressList'
+      }
+    ]
   },
 
   /**
