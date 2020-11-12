6837bc433a12bad5df58076b25e67688cd7e3ac7
路明 <snaillu@yeah.net>
2020-11-12 09:42:27 +0800

    搜索页面开发

---
+++ miniprogram/pages/search/search.js
@@ -0,0 +1,91 @@
+//Page Object
+Page({
+  data: {
+    hotWords: ['iphone 11', '华为Mate 40', '天猫双11', '京东双11']
+  },
+
+  // 删除搜索记录
+  async deleteSearchRecords(){        
+    let res = await wx.showModal({ content:'确认要删除最近搜索记录吗？'});
+    if(res.confirm){
+        wx.removeStorageSync('searchRecord');
+        this.setData({
+          searchRecord: []
+        })
+    }                                               
+  },
+
+  // 搜索框输入
+  searchInput(e){
+      this.setData({                   
+          searchWords: e.detail.value.trim(),
+      })
+  },
+
+  // 搜索
+  submitSearch(e){
+    let { searchWords, searchRecord } = this.data;
+    if(searchWords.length>0){
+
+      //判断搜索历史中是否已经有该关键字
+      if(searchRecord.indexOf(searchWords)<0){
+        if(searchRecord.length<20){
+          searchRecord.unshift(searchWords);
+        }else{
+          searchRecord.pop();
+          searchRecord.unshift(searchWords);
+        }
+      }
+      
+      wx.setStorageSync('searchRecord', searchRecord);
+      let eventDetail = { value: searchWords }; // detail对象，提供给事件监听函数
+      this.triggerEvent('handlesearch', eventDetail)
+    }else{
+      wx.showToast({
+        title: '请输入要搜索的关键字',
+        icon: 'none',
+      });
+    }
+    
+  },
+
+  // 最近搜索和热搜词搜索
+  wordsSearch(e){
+    this.triggerEvent('handlesearch', e.currentTarget.dataset)
+  },
+
+  //options(Object)
+  onLoad: function(options){
+    this.setData({
+      searchRecord: wx.getStorageSync("searchRecord") || []
+    })
+  },
+  onReady: function(){
+    
+  },
+  onShow: function(){
+    
+  },
+  onHide: function(){
+
+  },
+  onUnload: function(){
+
+  },
+  onPullDownRefresh: function(){
+
+  },
+  onReachBottom: function(){
+
+  },
+  onShareAppMessage: function(){
+
+  },
+  onPageScroll: function(){
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item){
+
+  }
+});
